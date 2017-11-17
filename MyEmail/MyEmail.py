ea8216c14c2c3f8ed131402b96eda261278589a0
yjb007 <14339989@qq.com>
2017-11-17 19:23:32 +0800

    Update MyEmail.py
    支持收件人列表

--- MyEmail/MyEmail.py
+++ MyEmail/MyEmail.py
@@ -41,7 +41,7 @@ class myEmail(object):
         msg['To'] = to
         msg['from'] = '%s<%s>' %(smtp_user, smtp_user)
         msg['subject'] = subject
-
+	mail_to_list = to.strip(',').split(',')
         try:
             server.connect(smtp_host, smtp_port)
         except timeout:
@@ -57,7 +57,7 @@ class myEmail(object):
                 return ret
 
         try:
-            server.sendmail(smtp_user, to, msg.as_string())
+            server.sendmail(smtp_user, mail_to_list, msg.as_string())
             server.quit()
         except Exception, e:
             ret['result'] = False
